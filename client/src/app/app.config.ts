33807a540ff865418eef1f43eea88c3e3691e9a3
Fagbenro Oluwafikayomi Caleb <fagbenrocoa@gmail.com>
2024-01-18 12:52:15 +0100

    Did an update

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,10 +1,8 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { MatNativeDateModule } from '@angular/material/core';
-import { AuthService } from './auth/auth.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
